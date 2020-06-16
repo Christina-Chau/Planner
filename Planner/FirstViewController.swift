f7655429c3404e9659153cce8fb1e9e42855858a
Christina-Chau <christinachau5@gmail.com>
2020-06-16 11:13:39 -0400

    Initial Commit

---
+++ Planner/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Planner
+//
+//  Created by Christina Chau on 6/16/20.
+//  Copyright © 2020 Christina Chau. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Planner/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Planner
+//
+//  Created by Christina Chau on 6/16/20.
+//  Copyright © 2020 Christina Chau. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
